e500c2a645c54ea6104b46315d106f79cd802ae4
Rob Mandelings <rob@minetronic.com>
2022-12-07 11:35:43 +0100

    Added encode_board

--- app/server/src/game_indexing.py
+++ app/server/src/game_indexing.py
@@ -16,38 +16,59 @@ def encode_closure(closure: typing.Dict[str, float], closure_type: Closure):
     return " ".join(closure_encodings)
 
 
-def reachability_closure(board: chess.Board, piece) -> typing.Dict[str, float]:
+def reachability_closure(board: chess.Board, piece: chess.Piece) -> typing.Dict[str, float]:
     """
     Computes the reachability closure of a piece on the given board
     """
     pass
 
 
-def attack_closure(board: chess.Board, piece) -> typing.Dict[str, float]:
+def attack_closure(board: chess.Board, piece: chess.Piece) -> typing.Dict[str, float]:
     """
     Compute the attack closure of a piece on the given board
     """
     pass
 
 
-def defense_closure(board: chess.Board, piece) -> typing.Dict[str, float]:
+def defense_closure(board: chess.Board, piece: chess.Piece) -> typing.Dict[str, float]:
     """
     Compute the attack closure of a piece on the given board
     """
     pass
 
 
-def ray_attack_closure(board: chess.Board, piece) -> typing.Dict[str, float]:
+def ray_attack_closure(board: chess.Board, piece: chess.Piece) -> typing.Dict[str, float]:
     """
     Compute the attack closure of a piece on the given board
     """
     pass
 
 
-def encode_board(board: chess.Board) -> str:
-    for row in reversed(range(1, 9)):
-        for col in range(ord('A'), ord('H')):
-            pass
+def encode_board(board: chess.Board,
+                 use_reachability: bool = True,
+                 use_attack: bool = True,
+                 use_defense: bool = True,
+                 use_ray_attack: bool = True) -> str:
+    base_board_encoding = ""
+    closure_encodings = ""
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        base_board_encoding += f"{piece.symbol()}{square}"
+
+        if use_reachability:
+            r_closure_enc = reachability_closure(board, piece)
+            closure_encodings += f"{r_closure_enc}\n"
+        if use_attack:
+            a_closure_enc = attack_closure(board, piece)
+            closure_encodings += f"{a_closure_enc}\n"
+        if use_defense:
+            d_closure_enc = defense_closure(board, piece)
+            closure_encodings += f"{d_closure_enc}\n"
+        if use_ray_attack:
+            x_closure_enc = ray_attack_closure(board, piece)
+            closure_encodings += f"{x_closure_enc}\n"
+
+    return f"{base_board_encoding}\n{closure_encodings}"
 
 
 def index_games(games: typing.List[chess.pgn.Game], num_skip=12):

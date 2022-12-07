75421e61039db1e2b142ef6224bb10fa8295cba3
Rob Mandelings <rob@minetronic.com>
2022-12-07 09:40:05 +0100

    Chess api written

--- app/server/src/chess_api.py
+++ app/server/src/chess_api.py
@@ -1,14 +1,18 @@
+import chess
 from flask import request
 from flask_restful import Resource
+
+from game_indexing import retrieve
 
 
 class SearchResource(Resource):
 
     def get(self):
-        state = request.args['state']
+        fen = request.args['state']
         """
         Handles get requests for IR queries made by the user
         """
-        # result = parse.unquote(state_encoding)
-        return {'msg': state}
-        pass
+
+        board = chess.Board(fen)
+        results = retrieve(board)
+        return {'results': results}

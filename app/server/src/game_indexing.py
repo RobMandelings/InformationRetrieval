2dac17e04ed2e7c7916a3f80573daa871d3178d2
Rob Mandelings <rob@minetronic.com>
2022-12-07 11:48:21 +0100

    Added encoding for game board

--- app/server/src/game_indexing.py
+++ app/server/src/game_indexing.py
@@ -49,34 +49,36 @@ def encode_board(board: chess.Board,
                  use_attack: bool = True,
                  use_defense: bool = True,
                  use_ray_attack: bool = True) -> str:
-    base_board_encoding = ""
+    base_board_list = list()
     closure_encodings = ""
     for square in chess.SQUARES:
         piece = board.piece_at(square)
-        base_board_encoding += f"{piece.symbol()}{square}"
+        if piece:
+            base_board_list.append(f"{piece.symbol()}{chess.SQUARE_NAMES[square]}")
 
-        if use_reachability:
-            r_closure_enc = reachability_closure(board, piece)
-            closure_encodings += f"{r_closure_enc}\n"
-        if use_attack:
-            a_closure_enc = attack_closure(board, piece)
-            closure_encodings += f"{a_closure_enc}\n"
-        if use_defense:
-            d_closure_enc = defense_closure(board, piece)
-            closure_encodings += f"{d_closure_enc}\n"
-        if use_ray_attack:
-            x_closure_enc = ray_attack_closure(board, piece)
-            closure_encodings += f"{x_closure_enc}\n"
+            if use_reachability:
+                r_closure_enc = reachability_closure(board, piece)
+                closure_encodings += f"{r_closure_enc}\n"
+            if use_attack:
+                a_closure_enc = attack_closure(board, piece)
+                closure_encodings += f"{a_closure_enc}\n"
+            if use_defense:
+                d_closure_enc = defense_closure(board, piece)
+                closure_encodings += f"{d_closure_enc}\n"
+            if use_ray_attack:
+                x_closure_enc = ray_attack_closure(board, piece)
+                closure_encodings += f"{x_closure_enc}\n"
 
+    base_board_encoding = " ".join(base_board_list)
     return f"{base_board_encoding}\n{closure_encodings}"
 
 
-def index_games(games: typing.List[chess.pgn.Game], num_skip:int = 12):
+def index_games(games: typing.List[chess.pgn.Game], num_skip: int = 12):
     """
     Base algorithm of the paper
     games: list of games
     """
-    documents = [] # TODO instead of return list add to documents index
+    documents = []  # TODO instead of return list add to documents index
     for g in games:
         # numskip
         board = g.board()
@@ -87,12 +89,11 @@ def index_games(games: typing.List[chess.pgn.Game], num_skip:int = 12):
 
     return documents
 
-
-# Test for index_games
-pgn = open("example_games/game.pgn")
-game = chess.pgn.read_game(pgn)
-games = [game]
-index_games(games)
+    # Test for index_games
+    pgn = open("example_games/game.pgn")
+    game = chess.pgn.read_game(pgn)
+    games = [game]
+    index_games(games)
 
 
 def retrieve(board: chess.Board):
@@ -100,15 +101,13 @@ def retrieve(board: chess.Board):
     Retrieves a ranked list of game states provided the query
     TODO retrieve complete games as documents instead of boards
     """
-    board_encoding = encode_board(board)
+    board_encoding = encode_board(board, False, False, False, False)
     # add additional info to encoding with closures
     # retrieve from index (return for test)
     return board_encoding
-
 
 # TODO test max 1 state retrieved per game
 
 # TODO: Board matrix as dictionary
 
 # TODO: document the board encoding from paper
-

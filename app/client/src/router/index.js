85ee289dcc7b9fe3dafc94f3fc21e778b3054e8c
Rob Mandelings <rob@minetronic.com>
2022-12-14 17:39:25 +0100

    Removed different pages, now chess page is the default

--- app/client/src/router/index.js
+++ app/client/src/router/index.js
@@ -1,12 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
-import Home from '@/views/Home.vue'
 import Chess from "@/views/Chess.vue";
-import Ping from '@/views/Ping.vue';
 
 const routes = [
-    {path: '/', name: 'Home', component: Home},
-    {path: '/ping', name: 'Ping', component: Ping},
-    {path: '/chess', name: 'Chess', component: Chess}
+    {path: '/', name: 'Chess', component: Chess},
 ]
 
 const router = createRouter({
